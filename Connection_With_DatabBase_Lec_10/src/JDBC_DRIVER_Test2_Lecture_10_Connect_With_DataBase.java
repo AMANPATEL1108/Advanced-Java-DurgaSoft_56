32f41cd3cd563e1101c345c16b0443c8ef8a607b
AMANPATEL <amanpatel8844382@gmail.com>
2024-12-04 21:42:44 +0530

    first commit

---
+++ Connection_With_DatabBase_Lec_10/src/JDBC_DRIVER_Test2_Lecture_10_Connect_With_DataBase.java
@@ -0,0 +1,26 @@
+import java.sql.*;
+import java.util.*;
+import  sun.jdbc.odbc.*;
+
+public class JDBC_DRIVER_Test2_Lecture_10_Connect_With_DataBase {
+    public static void main(String[] args) throws SQLException {
+
+        // Remove the usage of sun.jdbc.odbc.JdbcOdbcDriver (deprecated in modern Java)
+        // Use a valid JDBC driver for the database you are connecting to.
+        // Here, we just demonstrate a generic Driver object registration
+
+        Driver driver = new com.mysql.cj.jdbc.Driver();  // Example driver, replace with your actual driver
+        try{
+            DriverManager.registerDriver(driver);
+
+        }catch (SQLException sql){
+        }
+
+        Enumeration<Driver> e = DriverManager.getDrivers();
+        while (e.hasMoreElements()) {
+            Driver d = e.nextElement();
+            System.out.println(d.getClass().getName());
+        }
+
+    }
+}
---
+++ Connection_With_DatabBase_Lec_10/src/JDBC_DRIVER_Test_Lecture_10_Connect_With_DataBase.java
@@ -0,0 +1,24 @@
+import java.sql.*;
+import java.util.*;
+
+public class JDBC_DRIVER_Test_Lecture_10_Connect_With_DataBase {
+    public static void main(String[] args) {
+
+        // Using Class.forName with MySQL JDBC Driver
+        try {
+            // MySQL JDBC driver class
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("MySQL JDBC driver successfully loaded.");
+
+        } catch (ClassNotFoundException cnfe) {
+            cnfe.printStackTrace();
+        }
+
+        // Listing registered JDBC drivers
+        Enumeration<Driver> e = DriverManager.getDrivers();
+        while (e.hasMoreElements()) {
+            Driver d = e.nextElement();
+            System.out.println(d.getClass().getName());
+        }
+    }
+}
